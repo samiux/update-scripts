8ca60fb268469deaad44e6ee5e2d0e8ceeb75205
Samiux <infosecninjas@gmail.com>
2021-09-01 03:40:46 +0800

    Add files via upload

---
+++ generator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+input="malicious.url"
+n=100000000
+x=2
+
+while IFS= read -r domain; do
+	printf 'reject dns any any -> any any (msg: "DNS %s"; dns.query; content:"%s"; nocase; pcre:"/%s$/"; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	n=$((n+1))
+	printf 'reject http any any -> any any (msg: "HTTP %s"; http.uri; content:"%s"; nocase; pcre:"/%s$/"; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	n=$((n+1))
+	printf 'reject tls any any -> any any (msg: "SNI %s"; tls.sni; content:"%s"; nocase; pcre:"/%s$/"; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	n=$((n+1))
+	printf 'reject tls any any -> any any (msg: "SSL %s"; tls.cert_subject; content:"%s"; nocase; pcre:"/%s$/"; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	n=$((n+1))
+done < "$input"
