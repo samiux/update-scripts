df508d56c8dc7c400100a828838a9759bac07e60
Samiux <infosecninjas@gmail.com>
2022-11-28 23:54:54 +0800

    Add files via upload

--- generator.sh
+++ generator.sh
@@ -2,7 +2,7 @@
 
 input="malicious.url"
 n=100000000
-x=76
+x=77
 
 while IFS= read -r domain; do
 	printf 'drop dns any any -> any any (msg: "DNS %s"; dns.query; content:"%s"; nocase; pcre:"/%s$/"; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
