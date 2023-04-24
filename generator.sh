529c4272da13659ca49a659aab8f81599f6292fa
Samiux <infosecninjas@gmail.com>
2023-04-24 21:02:08 +0800

    Add files via upload

--- generator.sh
+++ generator.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-input="malicious.url"
+input="phishings.txt"
 n=100000000
-x=183
+x=184
 
 while IFS= read -r domain; do
-	printf 'drop dns any any -> any any (msg: "DNS %s"; dns.query; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	printf 'drop dns any any -> any any (msg: "DNS Phishing %s"; dns.query; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
 	n=$((n+1))
-	printf 'drop http any any -> any any (msg: "HTTP %s"; http.uri; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	printf 'drop http any any -> any any (msg: "HTTP Phishing %s"; http.uri; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
 	n=$((n+1))
-	printf 'drop tls any any -> any any (msg: "SNI %s"; tls.sni; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	printf 'drop tls any any -> any any (msg: "SNI Phishing %s"; tls.sni; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
 	n=$((n+1))
-	printf 'drop tls any any -> any any (msg: "SSL %s"; tls.cert_subject; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
+	printf 'drop tls any any -> any any (msg: "SSL Phishing %s"; tls.cert_subject; content:"%s"; nocase; pcre:"/%s$/"; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain" "$domain" "$domain" "$n" "$x" >> malicious.rules
 	n=$((n+1))
 done < "$input"
