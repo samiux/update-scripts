7c995cd3a3f9a20f389c930d77ed525103584710
Samiux <infosecninjas@gmail.com>
2023-05-15 18:24:10 +0800

    Add files via upload

--- malicious.sh
+++ malicious.sh
@@ -2,7 +2,7 @@
 
 input="malicious.txt"
 n=100000000
-x=193
+x=194
 
 while IFS="," read -r domain urlcontent; do
 	printf 'drop http any any -> any any (msg: "Malicious URL %s"; http.header; content:"%s"; http.uri; content:"%s"; nocase; priority:1; classtype:policy-violation; sid: %d; rev:%s;)\n' "$domain$urlcontent" "$domain" "$urlcontent" "$n" "$x" >> malicious.rules
