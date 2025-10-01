a88652a949da4429a6ec9faba974ef42d4657691
Samiux <infosecninjas@gmail.com>
2025-10-02 04:08:47 +0800

    Add files via upload

--- remove-threat.py
+++ remove-threat.py
@@ -1,13 +1,11 @@
-# Copyright (C) 2015-2025, Wazuh Inc.
+#!/usr/bin/python3
+# Copyright (C) 2015-2022, Wazuh Inc.
 # All rights reserved.
 
 import os
 import sys
 import json
 import datetime
-import stat
-import tempfile
-import pathlib
 
 if os.name == 'nt':
     LOG_FILE = "C:\\Program Files (x86)\\ossec-agent\\active-response\\active-responses.log"
@@ -32,39 +30,47 @@ def write_debug_file(ar_name, msg):
         log_file.write(str(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')) + " " + ar_name + ": " + msg +"\n")
 
 def setup_and_check_message(argv):
+
+    # get alert from stdin
     input_str = ""
     for line in sys.stdin:
         input_str = line
         break
 
-    msg_obj = message()
+
     try:
         data = json.loads(input_str)
     except ValueError:
         write_debug_file(argv[0], 'Decoding JSON has failed, invalid input format')
-        msg_obj.command = OS_INVALID
-        return msg_obj
+        message.command = OS_INVALID
+        return message
 
-    msg_obj.alert = data
+    message.alert = data
+
     command = data.get("command")
 
     if command == "add":
-        msg_obj.command = ADD_COMMAND
+        message.command = ADD_COMMAND
     elif command == "delete":
-        msg_obj.command = DELETE_COMMAND
+        message.command = DELETE_COMMAND
     else:
-        msg_obj.command = OS_INVALID
+        message.command = OS_INVALID
         write_debug_file(argv[0], 'Not valid command: ' + command)
 
-    return msg_obj
+    return message
+
 
 def send_keys_and_check_message(argv, keys):
+
+    # build and send message with keys
     keys_msg = json.dumps({"version": 1,"origin":{"name": argv[0],"module":"active-response"},"command":"check_keys","parameters":{"keys":keys}})
+
     write_debug_file(argv[0], keys_msg)
 
     print(keys_msg)
     sys.stdout.flush()
 
+    # read the response of previous message
     input_str = ""
     while True:
         line = sys.stdin.readline()
@@ -72,47 +78,31 @@ def send_keys_and_check_message(argv, keys):
             input_str = line
             break
 
+    # write_debug_file(argv[0], input_str)
+
     try:
         data = json.loads(input_str)
     except ValueError:
         write_debug_file(argv[0], 'Decoding JSON has failed, invalid input format')
-        return OS_INVALID
+        return message
 
     action = data.get("command")
-    if action == "continue":
-        return CONTINUE_COMMAND
-    elif action == "abort":
-        return ABORT_COMMAND
+
+    if "continue" == action:
+        ret = CONTINUE_COMMAND
+    elif "abort" == action:
+        ret = ABORT_COMMAND
     else:
+        ret = OS_INVALID
         write_debug_file(argv[0], "Invalid value of 'command'")
-        return OS_INVALID
 
-def secure_delete_file(filepath_str, ar_name):
-    filepath = pathlib.Path(filepath_str)
-
-    # Reject NTFS alternate data streams
-    if '::' in filepath_str:
-        raise Exception(f"Refusing to delete ADS or NTFS stream: {filepath_str}")
-
-    # Reject symbolic links and reparse points
-    if os.path.islink(filepath):
-        raise Exception(f"Refusing to delete symbolic link: {filepath}")
-
-    attrs = os.lstat(filepath).st_file_attributes
-    if attrs & stat.FILE_ATTRIBUTE_REPARSE_POINT:
-        raise Exception(f"Refusing to delete reparse point: {filepath}")
-
-    resolved_filepath = filepath.resolve()
-
-    # Ensure it's a regular file
-    if not resolved_filepath.is_file():
-        raise Exception(f"Target is not a regular file: {resolved_filepath}")
-
-  # Perform deletion
-    os.remove(resolved_filepath)
+    return ret
 
 def main(argv):
+
     write_debug_file(argv[0], "Started")
+
+    # validate json and get command
     msg = setup_and_check_message(argv)
 
     if msg.command < 0:
@@ -123,7 +113,9 @@ def main(argv):
         keys = [alert["rule"]["id"]]
         action = send_keys_and_check_message(argv, keys)
 
+        # if necessary, abort execution
         if action != CONTINUE_COMMAND:
+
             if action == ABORT_COMMAND:
                 write_debug_file(argv[0], "Aborted")
                 sys.exit(OS_SUCCESS)
@@ -132,20 +124,17 @@ def main(argv):
                 sys.exit(OS_INVALID)
 
         try:
-            file_path = alert["data"]["virustotal"]["source"]["file"]
-            if os.path.exists(file_path):
-                secure_delete_file(file_path, argv[0])
-                write_debug_file(argv[0], json.dumps(msg.alert) + " Successfully removed threat")
-            else:
-                write_debug_file(argv[0], f"File does not exist: {file_path}")
+            os.remove(msg.alert["parameters"]["alert"]["data"]["virustotal"]["source"]["file"])
+            write_debug_file(argv[0], json.dumps(msg.alert) + " Successfully removed threat")
         except OSError as error:
             write_debug_file(argv[0], json.dumps(msg.alert) + "Error removing threat")
-        except Exception as e:
-            write_debug_file(argv[0], f"{json.dumps(msg.alert)}: Error removing threat: {str(e)}")
+
+
     else:
         write_debug_file(argv[0], "Invalid command")
 
     write_debug_file(argv[0], "Ended")
+
     sys.exit(OS_SUCCESS)
 
 if __name__ == "__main__":
